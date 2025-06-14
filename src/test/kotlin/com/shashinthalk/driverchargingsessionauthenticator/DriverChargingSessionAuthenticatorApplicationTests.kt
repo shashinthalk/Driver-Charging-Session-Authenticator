5861e6e476f69afb6e724dafdebf48e4f8b61f6e
shashinthalk <shashinthanishan00@gmail.com>
2025-06-14 15:33:01 +0200

    stable v 1.0.0

---
+++ src/test/kotlin/com/shashinthalk/driverchargingsessionauthenticator/DriverChargingSessionAuthenticatorApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.shashinthalk.driverchargingsessionauthenticator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DriverChargingSessionAuthenticatorApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
